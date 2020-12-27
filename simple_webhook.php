2979e3b032473d2291b82f0fba41e934ac2a2735
otrodemo <76624596+otrodemo@users.noreply.github.com>
2020-12-27 00:28:53 -0500

    Update simple_webhook.php

--- simple_webhook.php
+++ simple_webhook.php
@@ -1,9 +1,39 @@
 <?php
 if ( isset($_POST) ) { if (!empty($_POST) ) {
+	
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			
+			$fopen = fopen("request_log.txt","a+");
+			fwrite($fopen, var_export($_POST, true). "\n" .  date('Y-m-d H:i:s', time()).PHP_EOL . "\n\n" );
+			fclose($fopen);
+			
+			//file_put_contents("post.log", file_get_contents("post.log")."\n\n". var_export($_GET, true));
+/*
+			$raw_payload = file_get_contents('php://input');
+			$payload = json_decode($raw_payload, true);
+
+			if (is_array($payload)) {
+				$fh = fopen("log.txt", "a+");
+
+				if ($fh) {
+					fwrite($fh, date('Y-m-d H:i:s', time()).PHP_EOL);
+					fclose($fh);
+				} else {
+					trigger_error("Unable to open file!");
+				}
+			} else {
+				trigger_error("Invalid payload!");
+			}
+		} else {
+			trigger_error("Invalid request!");
+		}//*/
 		
+		}
+		/*
 		$request = file_get_contents('php://input');
 		$req_dump = print_r( $request, true );
 		$fp = file_put_contents( 'request.log', $req_dump );
+		///*/
 		
 		header("HTTP/1.1 200 OK");
 		http_response_code(201);
